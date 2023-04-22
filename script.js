f5f776e6080c5735b3e8887c44eec7fc81a97829
Kuyne12 <124099764+Kuyne12@users.noreply.github.com>
2023-04-22 17:06:59 +0000

    updated project

---
+++ script.js
@@ -0,0 +1,22 @@
+let multiplicationTable = [];
+
+for(let i=1; i<11; i++) {
+    for(let j=1; j<11; j++) {
+        multiplicationTable.push{'${i} * ${j} = ${i*j}'}
+     } 
+}
+
+console.log(multiplicationTable)
+
+let prefix = ['big,' , 'important', 'very', 'cool', 'awesome'];
+let word = ['code', 'tech', 'coders', 'geeks', 'stack'];
+
+for(let i=0; i < prefix.length; i++) {
+    for(let j=0; j < word.length; j++) {
+        let div = document.createElement('div');
+        div.innerText = '
+            $(prefix[i]}${word[j].com
+
+        document.body.appendChild{div};
+    }
+}
